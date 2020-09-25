b2b38815ba8ef124dc63bc446e87495c2ca56022
Zoé Carvallo <zoecarvallo@gmail.com>
2020-09-25 14:33:21 +0200

    test

---
+++ src/js/daterangeBar.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function () {
+            $('#el').daterangeBar({
+                'endDate': '22-03-2021',
+                'privateColors': true
+            });
+        });
